8bcb9e89cef32b630c9d4641969df6ca5c444336
Justin Leclerc <justin_leclerc@hotmail.com>
2016-09-29 10:28:30 -0400

    going to test if worker runs

--- application.py
+++ application.py
@@ -2,6 +2,8 @@ import os
 from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
 from message_receiver import MessageReceiver
+from webserver.router import Router
+from assessment.assessment_worker import AssessmentWorker
 import twilio.twiml	
 import pprint
 import time
@@ -11,62 +13,18 @@ app = application
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['FRANCIS_DB_URI']
 db = SQLAlchemy(app)
 
-# Test the availability of Francis
-@app.route("/status", methods=['GET', 'POST'])
-def status ():
-	return "Francis is available"
-
-# @app.route("/assessment", methods=['POST'])
-# def assessment ():
-# 	message_receiver = MessageReceiver(db)
-# 	from_phone_number = '666'
-# 	text = "This is a test to see if the assessment daemon is running. I'm inside the /assessment route"
-# 	message_receiver.sms(from_phone_number, text)
-# 	return "/tasks/assessment returned"
-
-# Endpoint for Twilio sms messages
-@app.route("/twilio/sms", methods=['POST']) # endpoint for Twilio sms messages
-def twilio_sms():
-
-	message_receiver = MessageReceiver(db)
-	from_phone_number = request.values.get("From")
-	text = request.values.get("Body")
-	message_receiver.sms(from_phone_number, text)
-
-	return '/twilio/sms returned'
-
-@app.route("/simulator/sms", methods=['POST'])
-def simulator_sms():
-	message_receiver = MessageReceiver(db)
-	from_phone_number = request.values.get("From")
-	text = request.values.get("Body")
-	message_receiver.sms(from_phone_number, text)
-
-	return '/simulator/sms returned'
-
-	# TODO: find/create a logging system
-	# Log incoming message
-	# with open('sms_posts.log', 'a') as f:
-	# 	f.write("\n\n===================================\n\n")
-	# 	f.write(time.strftime("%c\n"))
-	# 	pprint.pprint(vars(request.values), stream=f)
-	# 	f.write("\n\n===================================\n\n")
-
-	# # Get the human from the DB
-	# from_human = db.session.query(Human).filter_by(phone_number=sms_from_phone_number).first()
-	
-	# # Create the human if it does not exist
-	# if from_human is None:
-	# 	from_human = Human(sms_from_phone_number)
-	# 	db.session.add(from_human)
-	# 	db.session.commit()
-
-	# # Insert the sms into the db
-	# message = Message(sms_text, from_human.id)
-	# db.session.add(message)
-	# db.session.commit()
-
-	# return output_html
+# Test to see if AWS will run the AssessmentWorker
+assessment_worker = AssessmentWorker(db)
+application = assessment_worker
 
 if __name__ == "__main__":
-    app.run()
+	application.run()
+
+# if __name__ == "__main__" and os.environ['FRANCIS_PROCESS_TYPE'] == 'WEBSERVER':
+# 	router = Router()
+# 	router.applyRoutes(app)
+# 	app.run()
+# elif __name__ == "__main__" and os.environ['FRANCIS_PROCESS_TYPE'] == 'ASSESSMENT_WORKER':
+# 	print ("Here I would run the Assessment Worker")
+# elif __name__ == "__main__":
+# 	print ("ERROR: unrecognized PROCESS_TYPE with __name == '__main__'")
