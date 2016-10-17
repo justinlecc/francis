ad3501f6679255a6178ebe167fc1dc490cd44ae8
Justin Leclerc <justin_leclerc@hotmail.com>
2016-10-16 23:30:15 -0400

    now trying to log into the app folder because don't seem to have permission anywhere else

--- application.py
+++ application.py
@@ -15,9 +15,8 @@ if os.environ['FRANCIS_ENV'] == 'LOCAL':
 	logging.basicConfig(filename=os.environ['FRANCIS_LOGFILE'], level=logging.DEBUG)
 	print("APP LOGGING TO " + os.environ['FRANCIS_LOGFILE'])
 else:
-	logging.basicConfig(filename='/home/ec2-user/francisapp.log', level=logging.DEBUG)
-	# print("APP LOGGING TO " + os.getcwd() + "/francisapp.log")
-	print("APP LOGGING TO " + "/home/ec2-user/francisapp.log")
+	logging.basicConfig(filename='francisapp.log', level=logging.DEBUG)
+	print("APP LOGGING TO " + os.getcwd() + "/francisapp.log")
 
 # Initialize app
 application = Flask(__name__)
