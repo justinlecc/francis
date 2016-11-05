754b937e703ab3fa7e7c01146cf0f22fb98aa720
Justin Leclerc <justin_leclerc@hotmail.com>
2016-11-05 00:11:10 -0400

    trying to log to stdout

--- application.py
+++ application.py
@@ -15,8 +15,10 @@ if os.environ['FRANCIS_ENV'] == 'LOCAL':
 	logging.basicConfig(filename=os.environ['FRANCIS_LOGFILE'], level=logging.DEBUG)
 	print("APP LOGGING TO " + os.environ['FRANCIS_LOGFILE'])
 else:
-	logging.basicConfig(filename=os.environ['FRANCIS_LOGFILE'], level=logging.DEBUG)
-	print("APP LOGGING TO " + os.environ['FRANCIS_LOGFILE'])
+	# logging.basicConfig(filename=os.environ['FRANCIS_LOGFILE'], level=logging.DEBUG)
+	# print("APP LOGGING TO " + os.environ['FRANCIS_LOGFILE'])
+	logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+	print("APP LOGGING TO stdout")
 
 # Initialize app
 application = Flask(__name__)
